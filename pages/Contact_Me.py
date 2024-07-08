4f6b8c529ce7a1d486b021dc925f4e369ce73d79
Indu <indulahari6@gmail.com>
2024-07-08 19:15:43 +0530

    Create send_mail function to send message

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,8 +1,16 @@
 import streamlit
+from send_gmail import send_mail
 
 with streamlit.form(key="email_form"):
-    streamlit.text_input("Your Email Address")
-    streamlit.text_area("Your Message")
+    user_mail = streamlit.text_input("Your Email Address")
+    raw_message = streamlit.text_area("Your Message")
+    message = f"""\
+    Subject: New mail from {user_mail}
+    From: {user_mail}
+    
+    {raw_message}
+    """
     button = streamlit.form_submit_button("Submit")
     if button:
+        send_mail(message)
         print("Your mail is sent successfully!")
--- send_gmail.py
+++ send_gmail.py
@@ -1,21 +1,18 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "indulahari6@gmail.com"
-password = ""
+def send_mail(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "indulahari6@gmail.com"
-message = """\
-Subject : Google Meet
+    username = "indulahari6@gmail.com"
+    password = ""
 
-Hey! Can u set a meeting?
-"""
+    receiver = "indulahari6@gmail.com"
 
-context = ssl.create_default_context()
+    context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL(host, port, context=context)as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context)as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
